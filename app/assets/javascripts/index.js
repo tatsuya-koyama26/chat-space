6275d6781657cbbd6239685b81244b0544ced1a3
tatsuya <tatsuya2258>
2019-05-28 15:24:49 +0900

    add: 非同期通信

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,3 +1,9 @@
 $("#user-search-field").on("keyup", function() {
   var input = $("#user-search-field").val();
+  $.ajax({
+    type: 'GET',
+    url: '/users',
+    data: { keyword: input },
+    dataType: 'json'
+  })
 });
