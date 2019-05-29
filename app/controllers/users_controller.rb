72ce2925524b23009ce672d87407f9a693f9a8a6
tatsuya <tatsuya2258>
2019-05-29 15:11:13 +0900

    update: .where.not(name: current_user.name)
    自分自身をインクリメンタルサーチの検索結果に表示しない

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?)',"%#{params[:keyword]}%").not(id: current_user.id)
+    @users = User.where('name LIKE(?)',"%#{params[:keyword]}%").where.not(name: current_user.name)
     respond_to do |format|
       format.json
     end
