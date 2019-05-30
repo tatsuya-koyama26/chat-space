05ff881a63bdaee557a5c81eff94b6f8bfd03298
tatsuya <tatsuya2258>
2019-05-30 11:59:59 +0900

    fixed: api::controller

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::MessagesController < ApplicationController
   def index
-    @message = Message.new
+    @group = Group.find(params[:group_id])
     @messages = @group.messages.includes(:user).where('id > ?', params[:id])
   end
 end
